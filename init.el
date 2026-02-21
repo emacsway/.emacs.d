2d00a620b445b0947d4c1f7dfb6a544889184c72
Ivan Zakrevsky <ivzak@yandex.ru>
2026-02-21 05:31:47 +0300

    first commit

---
+++ init.el
@@ -0,0 +1,110 @@
+;; nvm path
+(let ((nvm-node-dir (expand-file-name "~/.nvm/versions/node")))
+  (when (file-directory-p nvm-node-dir)
+    (let ((newest (car (last (directory-files nvm-node-dir t "^v")))))
+      (when newest
+        (let ((bin-dir (concat newest "/bin")))
+          (setenv "PATH" (concat bin-dir ":" (getenv "PATH")))
+          (add-to-list 'exec-path bin-dir))))))
+
+
+;; go path
+(let ((go-bin (expand-file-name "~/go/bin")))
+  (when (file-directory-p go-bin)
+    (setenv "PATH" (concat go-bin ":" (getenv "PATH")))
+    (add-to-list 'exec-path go-bin)))
+
+
+;; package
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; auto-install missing packages
+(defvar my-packages '(company pyvenv yasnippet web-mode go-mode))
+
+(defun my-install-packages ()
+  (package-refresh-contents)
+  (dolist (pkg my-packages)
+    (unless (package-installed-p pkg)
+      (package-install pkg))))
+
+(unless (cl-every #'package-installed-p my-packages)
+  (my-install-packages))
+
+
+;; company (completion UI)
+(require 'company)
+(global-company-mode 1)
+(setq company-dabbrev-ignore-case nil)
+(setq company-minimum-prefix-length 1)
+(setq company-tooltip-align-annotations t)
+
+
+;; eglot + pyright (built-in in Emacs 30)
+(require 'eglot)
+(add-to-list 'eglot-server-programs '(python-mode . ("pyright-langserver" "--stdio")))
+(add-hook 'python-mode-hook 'eglot-ensure)
+(add-hook 'python-mode-hook (lambda ()
+  (define-key python-mode-map (kbd "C-c g") 'xref-find-definitions)
+  (define-key python-mode-map (kbd "C-c d") 'eldoc-doc-buffer)
+  (define-key python-mode-map (kbd "C-c TAB") 'completion-at-point)
+  (define-key python-mode-map (kbd "C-c r") 'eglot-rename)
+  (define-key python-mode-map (kbd "C-c C-a") 'eglot-code-actions)
+  (define-key python-mode-map (kbd "C-c f") 'eglot-format-buffer)
+))
+
+;; ruff format on save
+(setq python-check-command "ruff check")
+(add-hook 'python-mode-hook (lambda ()
+  (add-hook 'before-save-hook 'eglot-format-buffer nil t)))
+
+;; ipython
+(when (executable-find "ipython")
+  (setq python-shell-interpreter "ipython"
+        python-shell-interpreter-args "--simple-prompt --pprint -i"))
+
+;; eglot + gopls
+(add-hook 'go-mode-hook 'eglot-ensure)
+(add-hook 'go-mode-hook (lambda ()
+  (define-key go-mode-map (kbd "C-c g") 'xref-find-definitions)
+  (define-key go-mode-map (kbd "C-c d") 'eldoc-doc-buffer)
+  (define-key go-mode-map (kbd "C-c TAB") 'completion-at-point)
+  (define-key go-mode-map (kbd "C-c r") 'eglot-rename)
+  (define-key go-mode-map (kbd "C-c C-a") 'eglot-code-actions)
+  (define-key go-mode-map (kbd "C-c f") 'eglot-format-buffer)
+  (add-hook 'before-save-hook 'eglot-format-buffer nil t)))
+
+
+;; pyvenv
+(require 'pyvenv)
+(setq pyvenv-default-virtual-env-name ".env")
+
+
+;; yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+
+;; web-mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(setq web-mode-engines-alist '(("django" . "\\.html\\'") ("django" . "\\.djhtml\\'")))
+(add-hook 'web-mode-hook (lambda ()
+  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-css-indent-offset 4)
+  (setq web-mode-code-indent-offset 4)
+  (setq web-mode-style-padding 0)
+  (setq web-mode-script-padding 0)
+  (setq web-mode-block-padding 0)))
+
+
+;; ido
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-flex-matching t)
+
+
+;; settings
+(load-file "~/.emacs.d/settings.el")
---
+++ settings.el
@@ -0,0 +1,79 @@
+;; M-x load-file /path-to-this-file/settings.el
+;; Simple include it from any environment.
+
+(defconst my-emacs-root
+  (file-name-directory (or load-file-name
+                           (when (boundp 'bytecomp-filename) bytecomp-filename)
+                           buffer-file-name))
+  "Installation directory of my-emacs"
+)
+
+(setq auto-save-default nil)
+(setq case-fold-search nil)
+(setq column-number-mode t)
+(setq delete-auto-save-files nil)
+(setq global-hl-line-mode t)
+(global-display-line-numbers-mode t)
+(setq indent-tabs-mode nil)
+(setq make-backup-files nil)
+(setq mouse-wheel-mode t)
+(setq mouse-wheel-scroll-amount (quote (1 ((shift) . 1) ((control)))))
+;; (setq py-pychecker-command (concat my-emacs-root "bin/pychecker.sh"))
+;; (setq py-pychecker-command-args (quote ("")))
+;; (setq python-check-command (concat my-emacs-root "bin/pychecker.sh"))
+(setq show-paren-mode t)
+(setq tab-width 4)
+(setq sgml-basic-offset 4)
+(setq global-subword-mode t)
+(setq tags-case-fold-search nil)
+(setq xterm-mouse-mode t)
+;; (setq ido-mode t)
+
+;; (xterm-mouse-mode)
+;; (gpm-mouse-mode)
+;; (mouse-wheel-mode)
+;; (ecb-activate)
+;; (tmm-menubar)
+
+(setq file-name-coding-system 'utf-8)
+(setq scroll-step 1)
+;; (setq ac-ignore-case nil) ;; auto-complete removed
+(setq require-final-newline t)
+(setq grep-find-command '("find . -name \"*.py\" -type f -print0 | xargs -0 -e grep -nH -e \"\"" . 93))
+(setq grep-find-ignored-directories '("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}"))
+(global-set-key (kbd "C-c m f") 'rgrep)
+;; (setq grep-find-command "find . ! -name \"*~\" ! -name \"#*#\" -type f -print0 | xargs -0 -e grep -nH -e ")
+
+;; occur
+(defun quick-occur-at-point ()
+  "look for word at point in buffer"
+  (interactive)
+  (occur (thing-at-point 'symbol)))
+
+(global-set-key (kbd "C-c m o") 'quick-occur-at-point)
+
+;; search
+(define-key isearch-mode-map (kbd "C-d")
+  'fc/isearch-yank-symbol)
+(defun fc/isearch-yank-symbol ()
+  "Yank the symbol at point into the isearch minibuffer.
+
+C-w does something similar in isearch, but it only looks for
+the rest of the word. I want to look for the whole string. And
+symbol, not word, as I need this for programming the most."
+  (interactive)
+  (isearch-yank-string
+   (save-excursion
+     (when (and (not isearch-forward)
+                isearch-other-end)
+       (goto-char isearch-other-end))
+     (thing-at-point 'symbol))))
+
+;; (set-face-attribute 'default nil :height 100)
+
+;; Buffers
+ (defun prev-window ()
+   (interactive)
+   (other-window -1))
+
+(define-key global-map (kbd "C-x O") 'prev-window) ;; C-x p is now project.el prefix in Emacs 28+
