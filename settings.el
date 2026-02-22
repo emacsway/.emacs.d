c790369771075376046a08b9e39f55f8d956713e
Ivan Zakrevsky <ivzak@yandex.ru>
2026-02-23 00:26:13 +0300

    Add ido

--- settings.el
+++ settings.el
@@ -75,3 +75,47 @@ symbol, not word, as I need this for programming the most."
    (other-window -1))
 
 (define-key global-map (kbd "C-x O") 'prev-window) ;; C-x p is now project.el prefix in Emacs 28+
+
+;; Ido mode with fuzzy matching
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-flex-matching t) ;; enable fuzzy matching
+;; (setq ido-case-fold t)
+(ido-toggle-case)
+(setq imenu-sort-function 'imenu--sort-by-name)
+
+;; http://chopmo.blogspot.com/2008/09/quickly-jumping-to-symbols.html
+(require 'cl-lib)
+(defun ido-goto-symbol ()
+  "Will update the imenu index and then use ido to select a
+   symbol to navigate to"
+  (interactive)
+  (imenu--make-index-alist)
+  (let ((name-and-pos '())
+        (symbol-names '()))
+    (cl-labels ((addsymbols (symbol-list)
+                  (when (listp symbol-list)
+                    (dolist (symbol symbol-list)
+                      (let ((name nil) (position nil))
+                        (cond
+                         ((and (listp symbol) (imenu--subalist-p symbol))
+                          (addsymbols symbol))
+
+                         ((listp symbol)
+                          (setq name (car symbol))
+                          (setq position (cdr symbol)))
+
+                         ((stringp symbol)
+                          (setq name symbol)
+                          (setq position (get-text-property 1 'org-imenu-marker symbol))))
+
+                        (unless (or (null position) (null name))
+                          (add-to-list 'symbol-names name)
+                          (add-to-list 'name-and-pos (cons name position))))))))
+      (addsymbols imenu--index-alist))
+    (let* ((selected-symbol (ido-completing-read "Symbol? " symbol-names))
+           (position (cdr (assoc selected-symbol name-and-pos))))
+      (goto-char position))))
+
+(global-set-key (kbd "C-c t") 'ido-goto-symbol)
+
